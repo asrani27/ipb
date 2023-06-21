fe6481884229a26b2a7efe69bbe85723a1e82d45
asrani <asrani.27@gmail.com>
2023-06-21 09:27:16 +0800

    f

--- resources/views/layouts/menu_admin.blade.php
+++ resources/views/layouts/menu_admin.blade.php
@@ -2,7 +2,7 @@
 <li class="{{ (request()->is('admin/bidang*')) ? 'active' : '' }}"><a href="/admin/bidang"><i class="fa fa-institution"></i> <span>Bidang</span></a></li>
 <li class="{{ (request()->is('admin/program*')) ? 'active' : '' }}"><a href="/admin/program"><i class="fa fa-list"></i> <span>Program</span></a></li>
 <li class="{{ (request()->is('admin/kegiatan*')) ? 'active' : '' }}"><a href="/admin/kegiatan"><i class="fa fa-list"></i> <span>Kegiatan</span></a></li>
-<li class="{{ (request()->is('admin/pptk*')) ? 'active' : '' }}"><a href="/admin/pptk"><i class="fa fa-users"></i> <span>PPTK</span></a></li>
+{{-- <li class="{{ (request()->is('admin/pptk*')) ? 'active' : '' }}"><a href="/admin/pptk"><i class="fa fa-users"></i> <span>PPTK</span></a></li> --}}
 {{-- <li class="{{ (request()->is('admin/validasi*')) ? 'active' : '' }}"><a href="/admin/validasi"><i class="fa fa-check"></i> <span>Validasi Penginputan</span></a></li> --}}
 <li class="{{ (request()->is('admin/laporan*')) ? 'active' : '' }}"><a href="/admin/laporan"><i class="fa fa-file"></i> <span>Laporan RFK</span></a></li>
 <li class="{{ (request()->is('admin/laptriwulan*')) ? 'active' : '' }}"><a href="/admin/laptriwulan"><i class="fa fa-file"></i> <span>Laporan Triwulan</span></a></li>
