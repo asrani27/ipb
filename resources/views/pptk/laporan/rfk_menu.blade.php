32b7b639c5e2288fd072b8ebf866eec30db56f05
asrani <asrani.27@gmail.com>
2024-06-04 13:31:37 +0800

    f

--- resources/views/pptk/laporan/rfk_menu.blade.php
+++ resources/views/pptk/laporan/rfk_menu.blade.php
@@ -1,6 +1,5 @@
 
 <a href="/pptk/laporanrfk/" class="btn bg-purple btn-sm btn-flat"><strong>Kembali</strong></a>  
-<a href="/pptk/laporanrfk/kirimdata/{{$bulan}}/{{$subkegiatan->id}}" class="btn btn-primary btn-sm btn-flat" onclick="return confirm('Apakah Yakin Ingin Dikirim?');"><strong><i class="fa fa-send"></i> Kirim Data</strong></a>  
 
 <a href="/pptk/laporanrfk/{{$subkegiatan->id}}/{{$tahun}}/{{$bulan}}/srp" class="btn btn-danger btn-sm btn-flat"><strong>Sr Pengantar</strong></a> 
 <a href="/pptk/laporanrfk/{{$subkegiatan->id}}/{{$tahun}}/{{$bulan}}/rfk" class="btn btn-danger btn-sm btn-flat"><strong>RFK</strong></a> 
@@ -10,6 +9,7 @@
 {{-- <a href="/pptk/laporanrfk/{{$subkegiatan->id}}/{{$tahun}}/{{$bulan}}/v" class="btn btn-warning btn-sm btn-flat"><strong>V</strong></a>  --}}
 <a href="/pptk/laporanrfk/{{$subkegiatan->id}}/{{$tahun}}/{{$bulan}}/fiskeu" class="btn btn-success btn-sm btn-flat"><strong>Fis Keu</strong></a>  
 @if ($status_kirim == null)
+<a href="/pptk/laporanrfk/kirimdata/{{$bulan}}/{{$subkegiatan->id}}" class="btn btn-primary btn-sm btn-flat" onclick="return confirm('Apakah Yakin Ingin Dikirim?');"><strong><i class="fa fa-send"></i> Kirim Data</strong></a>  
 @else
 <a href="#" class="btn btn-primary btn-sm btn-flat"><strong><i class="fa fa-check"></i> Terkirim</strong></a> 
 @endif
