88476490e5cabd1dee416b6e5f59fb3839487a2c
asrani <asrani.27@gmail.com>
2024-07-10 08:43:16 +0800

    f

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -79,7 +79,7 @@
 
 	<script>
 		// Set the date we're counting down to
-		var countDownDate = new Date("Jul 1, 2024 10:37:25").getTime();
+		var countDownDate = new Date("Jul 10, 2024 12:37:25").getTime();
 		
 		// Update the count down every 1 second
 		var x = setInterval(function() {
