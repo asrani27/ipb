d85e0e627c871faa4fa0e5cf4dd17c7e7326127c
asrani <asrani.27@gmail.com>
2025-02-07 11:02:18 +0800

    f

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -237,6 +237,7 @@ class BerandaController extends Controller
         $tahun = Carbon::now()->format('Y');
         //$status = BatasInput::where('is_aktif', 1)->first()->nama;
         $status = 'murni';
+        $pergeseran_ke = null;
         // if (Auth::user()->pptk->skpd->murni == 1) {
         //     $status = 'murni';
         //     $pergeseran_ke = null;
