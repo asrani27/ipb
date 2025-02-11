5ebd0c6dc3216a743af56a3855eda394d655e513
asrani <asrani.27@gmail.com>
2025-02-11 10:27:45 +0800

    f

--- resources/views/admin/laporan/index.blade.php
+++ resources/views/admin/laporan/index.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 @push('css')
-    
+
 @endpush
 @section('content')
 <section class="content">
   <div class="row">
     <div class="col-md-12">
       <div class="box box-primary">
-          <div class="box-header">
-            <h3 class="box-title"><i class="fa fa-clipboard"></i> Status Laporan RFK SKPD</h3>
-          </div>
-          <div class="box-body table-responsive text-sm">
-            <table class="table table-hover">
-              <tbody>
+        <div class="box-header">
+          <h3 class="box-title"><i class="fa fa-clipboard"></i> Status Laporan RFK SKPD</h3>
+        </div>
+        <div class="box-body table-responsive text-sm">
+          <table class="table table-hover">
+            <tbody>
               <tr class='bg-purple'>
                 <th class="text-center" rowspan=2>No</th>
                 <th rowspan=2 style="text-align: center">TAHUN</th>
@@ -72,12 +72,52 @@
                   <a href="/admin/laporan/2024/desember" class='btn btn-flat btn-xs bg-purple'>RFK</a>
                 </td>
               </tr>
-              
-              </tbody>
-              
-            </table>
-          </div>
-          <!-- /.box-body -->
+              <tr>
+                <td>2</td>
+                <td>2025</td>
+                <td>
+                  <a href="/admin/laporan/2025/januari" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/februari" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/maret" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/april" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/mei" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/juni" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/juli" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/agustus" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/september" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/oktober" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/november" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+                <td>
+                  <a href="/admin/laporan/2025/desember" class='btn btn-flat btn-xs bg-purple'>RFK</a>
+                </td>
+              </tr>
+
+            </tbody>
+
+          </table>
+        </div>
+        <!-- /.box-body -->
       </div>
     </div>
     {{-- <a href="/admin/laporan/2022" class="btn btn-primary"><strong>2022</strong></a>
