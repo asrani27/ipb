ec50e751f21b272bfac028b53c87ce4ac9909c61
asrani <asrani.27@gmail.com>
2025-02-11 09:19:40 +0800

    f

--- app/Imports/SubKegiatanImport.php
+++ app/Imports/SubKegiatanImport.php
@@ -19,10 +19,18 @@ class SubKegiatanImport implements ToModel, WithStartRow
         if (!isset($row[0]) || !isset($row[1])) {
             return null; // Lewati baris jika data tidak lengkap
         }
+        // Pecah string menjadi array berdasarkan titik
+        $parts = explode('.', $row[0]);
+
+        // Ambil bagian untuk "kegiatan" (8 digit pertama)
+        $kegiatan = implode('.', array_slice($parts, 0, 5));
+
+        // Ambil bagian untuk "program" (5 digit pertama)
+        $program = implode('.', array_slice($parts, 0, 3));
 
         return MasterSubKegiatan::firstOrCreate(
             ['kode' => $row[0]], // Cek berdasarkan kode_akun
-            ['kode' => $row[0], 'nama' => $row[1]]  // Jika belum ada, simpan nama_akun
+            ['kode_program' => $program, 'kode_kegiatan' => $kegiatan, 'kode' => $row[0], 'nama' => $row[1]]  // Jika belum ada, simpan nama_akun
         );
     }
     public function startRow(): int
