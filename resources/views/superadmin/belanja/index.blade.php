f9c122a98a4ee9e53c6066dbd56c3ec888ffd147
asrani <asrani.27@gmail.com>
2025-10-08 14:31:29 +0800

    f

--- resources/views/superadmin/belanja/index.blade.php
+++ resources/views/superadmin/belanja/index.blade.php
@@ -15,8 +15,9 @@
 
 
       <div class="box-body">
-        <a href="/superadmin/belanjaskpd/export" class="btn btn-primary btn-block">Export
-          Laporan Belanja</a>
+        <a href="/superadmin/belanjaskpd/export" class="btn btn-primary btn-block" id="exportLink" onclick="handleExportClick(event)">
+          <i class="fa fa-download"></i> Export Laporan Belanja
+        </a>
       </div>
       <!-- /.box-body -->
     </div>
@@ -24,7 +25,61 @@
   </div>
 </div>
 
-@endsection
 @push('js')
-
+<script>
+function handleExportClick(event) {
+    event.preventDefault();
+    
+    const link = document.getElementById('exportLink');
+    const originalHref = link.href;
+    const originalContent = link.innerHTML;
+    
+    // Disable link dan tampilkan loading state
+    link.href = '#';
+    link.style.pointerEvents = 'none';
+    link.style.opacity = '0.6';
+    link.innerHTML = '<i class="fa fa-spinner fa-spin"></i> Sedang Memproses...';
+    
+    // Mulai download
+    window.location.href = originalHref;
+    
+    // Deteksi ketika download selesai menggunakan focus event
+    // Browser akan kehilangan focus saat download dialog muncul
+    // dan mendapatkan focus kembali setelah dialog ditutup
+    let downloadStarted = false;
+    let focusTimer;
+    
+    // Monitor focus changes
+    function checkDownloadComplete() {
+        if (document.hasFocus() && downloadStarted) {
+            // Download selesai, enable kembali link
+            link.href = originalHref;
+            link.style.pointerEvents = 'auto';
+            link.style.opacity = '1';
+            link.innerHTML = originalContent;
+            clearInterval(focusTimer);
+        } else if (!downloadStarted && document.hasFocus()) {
+            // Download belum dimulai, tunggu focus loss
+            setTimeout(() => {
+                if (!document.hasFocus()) {
+                    downloadStarted = true;
+                }
+            }, 100);
+        }
+    }
+    
+    // Mulai monitoring focus
+    focusTimer = setInterval(checkDownloadComplete, 500);
+    
+    // Fallback: enable setelah 30 detik jika tidak terdeteksi
+    setTimeout(() => {
+        clearInterval(focusTimer);
+        link.href = originalHref;
+        link.style.pointerEvents = 'auto';
+        link.style.opacity = '1';
+        link.innerHTML = originalContent;
+    }, 30000);
+}
+</script>
 @endpush
+@endsection
